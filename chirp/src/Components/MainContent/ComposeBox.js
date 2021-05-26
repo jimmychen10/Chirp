a538516c77f430e5a7cd37b99d1fc4bacc456829
Jimmy Chen <jimmychen1996@yahoo.com>
2021-05-25 22:27:41 -0700

    Completed Compose Box styling and made input box contenteditable

--- chirp/src/Components/MainContent/ComposeBox.js
+++ chirp/src/Components/MainContent/ComposeBox.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React,{useState}  from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import "../CSS/ComposeBox.scss"
-
+import ContentEditable from 'react-contenteditable'
 
 
 export default function ComposeBox() {
+
+    const [temp, setTemp] = useState("What's happening?")
     return (
         <div className = "composeBox">
             <div className = "composeBox_img">
@@ -26,15 +28,27 @@ export default function ComposeBox() {
                 
                 
                 <form className = "composeBox_form">
-                    <input 
+                    {/* <input 
                         type="text"
                         placeholder = "What's happening?"
                         className = "composeBox_form_input"
                         contentEditable= {true}
                         
+                    /> */}
+                    <ContentEditable
+                        className = "composeBox_form_input"
+                        // html = {temp}
+                        placeholder = "What's happening?"
                     />
-                    <div>
-                        <Button variant="outlined" className="composeBox_form_button" borderRadius="50%">Search</Button>
+                    
+                    <div  className = "composeBox_form_bottom">
+                        {/* <Button variant="outlined" className="composeBox_form_bottom_button" borderRadius="50%"><h2>button</h2></Button> */}
+                        <div className="composeBox_form_bottom_button">
+                            <span>
+                                <b>Chirp</b>
+                                
+                            </span>
+                        </div>
                     </div>
                     
                 </form>
